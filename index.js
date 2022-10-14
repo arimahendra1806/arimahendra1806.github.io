8091bcabcd37b994e6cc55f04fb22b9dcf74e565
Arimahendra1806 <arimahendra1806@gmail.com>
2022-10-14 12:43:35 +0700

    change

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-import initScrollReveal from "scripts/scrollReveal";
-import initTiltEffect from "scripts/tiltAnimation";
-import { targetElements, defaultProps } from "data/scrollRevealConfig";
+// import initScrollReveal from "scripts/scrollReveal";
+// import initTiltEffect from "scripts/tiltAnimation";
+// import { targetElements, defaultProps } from "data/scrollRevealConfig";
+
+const {initScrollReveal} = require('scripts/scrollReveal');
+const {initTiltEffect} = require('scripts/tiltAnimation');
+const {targetElements, defaultProps} = require('data/scrollRevealConfig');
+// const defaultProps = require('./data/scrollRevealConfig');
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
