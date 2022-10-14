000d9f7e51a0df0b8f70bf79e2181001cc0aa6a1
Arimahendra1806 <arimahendra1806@gmail.com>
2022-10-14 12:55:28 +0700

    up

--- index.js
+++ index.js
@@ -2,10 +2,96 @@
 // import initTiltEffect from "scripts/tiltAnimation";
 // import { targetElements, defaultProps } from "data/scrollRevealConfig";
 
-const {initScrollReveal} = require('scripts/scrollReveal.js');
-const {initTiltEffect} = require('scripts/tiltAnimation.js');
-const {targetElements, defaultProps} = require('data/scrollRevealConfig.js');
+// const {initScrollReveal} = require('scripts/scrollReveal.js');
+// const {initTiltEffect} = require('scripts/tiltAnimation.js');
+// const {targetElements, defaultProps} = require('data/scrollRevealConfig.js');
 // const defaultProps = require('./data/scrollRevealConfig');
+import VanillaTilt from "vanilla-tilt";
+
+function initScrollReveal(targetElements, defaultProps) {
+    if (!targetElements.length) return;
+  
+    ScrollReveal({ reset: false });
+  
+    targetElements.forEach(({ element, animation }) => {
+      ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
+    });
+}
+
+function initTiltAnimation() {
+  const elements = document.querySelectorAll(".js-tilt");
+  VanillaTilt.init(elements);
+}
+
+defaultProps = {
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    distance: "30px",
+    duration: 1000,
+    desktop: true,
+    mobile: true,
+  };
+  
+targetElements = [
+    {
+      element: ".section-title",
+      animation: {
+        delay: 300,
+        distance: "0px",
+        origin: "bottom",
+      },
+    },
+    {
+      element: ".hero-title",
+      animation: {
+        delay: 500,
+        origin: window.innerWidth > 768 ? "left" : "bottom",
+      },
+    },
+    {
+      element: ".hero-cta",
+      animation: {
+        delay: 1000,
+        origin: window.innerWidth > 768 ? "left" : "bottom",
+      },
+    },
+    {
+      element: ".about-wrapper__image",
+      animation: {
+        delay: 600,
+        origin: "bottom",
+      },
+    },
+    {
+      element: ".about-wrapper__info",
+      animation: {
+        delay: 1000,
+        origin: window.innerWidth > 768 ? "left" : "bottom",
+      },
+    },
+    {
+      element: ".project-wrapper__text",
+      animation: {
+        delay: 500,
+        origin: window.innerWidth > 768 ? "left" : "bottom",
+      },
+    },
+    {
+      element: ".project-wrapper__image",
+      animation: {
+        delay: 1000,
+        origin: window.innerWidth > 768 ? "right" : "bottom",
+      },
+    },
+    {
+      element: ".contact-wrapper",
+      animation: {
+        delay: 800,
+        origin: "bottom",
+      },
+    },
+  ];
+  
+
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
